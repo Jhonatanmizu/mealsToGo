116708e78bfdb6e5ad68d045b0eea2df66551da3
jhonatanmizu <natanjesuss20@gmail.com>
2022-11-23 21:18:36 -0300

    :recycle: added LayoutProps to styled card

--- src/layout/card.ts
+++ src/layout/card.ts
@@ -1,3 +1,4 @@
+import { LayoutProps } from "./../models/LayoutProps";
 import {
   Card,
   CardContentProps,
@@ -6,24 +7,21 @@ import {
   CardTitleProps,
 } from "react-native-paper";
 import styled from "styled-components/native";
-interface Props {
-  color?: string;
-  size?: number;
-  padding?: number;
-  borderRadius?: number;
-}
-export const RestaurantCard = styled(Card)<Props & CardProps>`
+
+export const RestaurantCard = styled(Card)<LayoutProps & CardProps>`
   background-color: ${({ color }) => color || "white"};
 `;
-export const RestaurantTitle = styled(Card.Title)<Props & CardTitleProps>`
+export const RestaurantTitle = styled(Card.Title)<LayoutProps & CardTitleProps>`
   background-color: ${({ color }) => color || "white"};
 `;
-export const RestaurantCardCover = styled(Card.Cover)<Props & CardCoverProps>`
+export const RestaurantCardCover = styled(Card.Cover)<
+  LayoutProps & CardCoverProps
+>`
   background-color: ${({ color }) => color || "white"};
   padding: ${({ padding }) => padding || "20px"};
 `;
 export const RestaurantCardContent = styled(Card.Content)<
-  Props & CardContentProps
+  LayoutProps & CardContentProps
 >`
   background-color: ${({ color }) => color || "white"};
 `;
