63e9bccef2613673a0e41c93cc2668da872f0151
jhonatanmizu <natanjesuss20@gmail.com>
2022-11-23 23:06:09 -0300

    :sparkles: created wrapper and added rating to card

--- src/features/restaurant/components/restaurant-info.tsx
+++ src/features/restaurant/components/restaurant-info.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { StyleSheet, Text } from "react-native";
-import { Card } from "react-native-paper";
+import Svg, { SvgXml } from "react-native-svg";
+
 import {
   RestaurantCard,
   RestaurantCardContent,
   RestaurantCardCover,
   RestaurantTitle,
 } from "../../../layout/card";
-import { Title } from "../../../layout/typography";
+import { StarsContainer, Wrapper } from "../../../layout/container";
+import { Caption, Paragraph, Subtitle } from "../../../layout/typography";
 import { RestaurantData } from "../../../models/RestaurantData";
-
+import star from "../../../assets/star";
 interface Props {
   restaturantInfo?: RestaurantData;
 }
@@ -18,20 +19,39 @@ export default function RestaurantInfo({ restaturantInfo = {} }: Props) {
     name = "Some rest",
     icon = "temp",
     photos = [
-      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+      "https://images.unsplash.com/photo-1621605815971-fbc98d665033?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
     ],
-    address = "",
+    address = "Citybank",
     isOpenNow = true,
     rating = 5,
     isClosed = false,
   } = restaturantInfo;
+  const ratingArray = Array.from(new Array(Math.ceil(rating)));
+
   return (
-    <RestaurantCard elevation={5}>
-      <RestaurantTitle title="Algo" subtitle="Somenthing" />
-      <RestaurantCardCover source={{ uri: photos[0] }} key={name} />
-      <RestaurantCardContent>
-        <Title>Man</Title>
-      </RestaurantCardContent>
-    </RestaurantCard>
+    <>
+      <RestaurantCard elevation={5}>
+        <RestaurantTitle title="Algo" subtitle="Somenthing" />
+        <RestaurantCardCover source={{ uri: photos[0] }} key={name} />
+        <RestaurantCardContent>
+          <Wrapper>
+            <Subtitle>{name}</Subtitle>
+            <Caption>{address}</Caption>
+            <StarsContainer>
+              {ratingArray.map(() => {
+                return <SvgXml height="20" width="20" xml={star} />;
+              })}
+            </StarsContainer>
+            <Paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse
+            </Paragraph>
+          </Wrapper>
+        </RestaurantCardContent>
+      </RestaurantCard>
+    </>
   );
 }
--- src/features/restaurant/screens/restaurants-screen.tsx
+++ src/features/restaurant/screens/restaurants-screen.tsx
@@ -15,6 +15,9 @@ export default function RestaurantsScreen() {
       </SearchContainer>
       <ListContainer>
         <RestaurantInfo />
+        <RestaurantInfo />
+        <RestaurantInfo />
+        <RestaurantInfo />
       </ListContainer>
     </SafeView>
   );
