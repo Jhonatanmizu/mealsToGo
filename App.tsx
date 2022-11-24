9440f2d08f2d52bc96be233ddb1e0019d09d6827
jhonatanmizu <natanjesuss20@gmail.com>
2022-11-23 23:05:28 -0300

    :sparkles: added fonts to app

--- App.tsx
+++ App.tsx
@@ -2,12 +2,25 @@ import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 
 import RestaurantsScreen from "./src/features/restaurant/screens/restaurants-screen";
-
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/infrastructure/theme/index";
+import { useFonts, Oswald_400Regular } from "@expo-google-fonts/oswald";
+import { Lato_400Regular, useFonts as useLato } from "@expo-google-fonts/lato";
 export default function App() {
+  const [oswaldLoaded] = useFonts({
+    Oswald_400Regular,
+  });
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
   return (
     <>
-      <RestaurantsScreen />
-      <ExpoStatusBar style="auto" />
+      {oswaldLoaded && latoLoaded && (
+        <ThemeProvider theme={theme}>
+          <RestaurantsScreen />
+          <ExpoStatusBar style="auto" />
+        </ThemeProvider>
+      )}
     </>
   );
 }
