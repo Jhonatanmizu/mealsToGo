b91d18b478269934e3281997f53e1235c79e04b6
jhonatanmizu <natanjesuss20@gmail.com>
2022-11-23 21:18:49 -0300

    :lipstick: created theme

--- src/layout/typography.ts
+++ src/layout/typography.ts
@@ -1,10 +1,7 @@
+import { LayoutProps } from "./../models/LayoutProps";
 import styled from "styled-components/native";
-interface Props {
-  color?: string;
-  size?: number;
-  padding?: number;
-}
-export const Title = styled.Text<Props>`
+
+export const Title = styled.Text<LayoutProps>`
   font-size: ${({ size }) => size || "16px"};
   padding: ${({ padding }) => padding || "16px"};
   color: ${({ color }) => color || "black"};
