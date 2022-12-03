51b8a0120c98592a1ba071d36d05b0d0a440c36c
jhonatanmizu <natanjesuss20@gmail.com>
2022-12-03 00:56:29 -0300

    :recycle: Added context to app

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import { theme } from "./src/infrastructure/theme/index";
 import { useFonts, Oswald_400Regular } from "@expo-google-fonts/oswald";
 import { Lato_400Regular, useFonts as useLato } from "@expo-google-fonts/lato";
 import AppNavigation from "./src/router/AppNavigation/AppNavigation";
+import { RestaurantsProvider } from "./src/contexts/RestaurantsContext";
 export default function App() {
   const [oswaldLoaded] = useFonts({
     Oswald_400Regular,
@@ -17,8 +18,9 @@ export default function App() {
     <>
       {oswaldLoaded && latoLoaded && (
         <ThemeProvider theme={theme}>
-          {/* <RestaurantsScreen /> */}
-          <AppNavigation />
+          <RestaurantsProvider>
+            <AppNavigation />
+          </RestaurantsProvider>
           <ExpoStatusBar style="auto" />
         </ThemeProvider>
       )}
