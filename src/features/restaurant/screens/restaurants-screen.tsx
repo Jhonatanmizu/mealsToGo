8e9b8b88aee9823f1d56007e28561a3b43edaecf
jhonatanmizu <natanjesuss20@gmail.com>
2022-12-03 00:56:12 -0300

    :sparkles: Added restaurants to restaurantsScreen

--- src/features/restaurant/screens/restaurants-screen.tsx
+++ src/features/restaurant/screens/restaurants-screen.tsx
@@ -8,20 +8,31 @@ import {
 } from "../../../layout/container";
 import { FlatList, StyleSheet } from "react-native";
 import { RestaurantData } from "../../../models/RestaurantData";
+import { useRestaurants } from "../../../contexts/RestaurantsContext";
+import { ActivityIndicator } from "react-native-paper";
 
 export default function RestaurantsScreen() {
+  const { restaurants, isLoading } = useRestaurants();
+  console.log(restaurants);
+
   return (
-    <SafeView>
-      <SearchContainer>
-        <Search placeholder="Busque aqui" />
-      </SearchContainer>
-      <FlatList
-        data={[{ name: "1" }, { name: "2" }, { name: "3" }]}
-        renderItem={() => <RestaurantInfo />}
-        keyExtractor={({ name }: RestaurantData) => name}
-        contentContainerStyle={styles.list}
-      />
-    </SafeView>
+    <>
+      <SafeView>
+        <SearchContainer>
+          <Search placeholder="Busque aqui" />
+        </SearchContainer>
+        {isLoading ? (
+          <ActivityIndicator animating />
+        ) : (
+          <FlatList
+            data={restaurants}
+            renderItem={({ item }) => <RestaurantInfo restaturantInfo={item} />}
+            keyExtractor={({ name }: RestaurantData) => name}
+            contentContainerStyle={styles.list}
+          />
+        )}
+      </SafeView>
+    </>
   );
 }
 const styles = StyleSheet.create({
